7629b381cc0392a21b83c7e3d14997bfe23e5df7
Felipe Zambrin <zambrinf@gmail.com>
2020-10-12 16:05:25 -0300

    Service layer, component registration

--- src/main/java/com/felipezambrin/course/resources/UserResource.java
+++ src/main/java/com/felipezambrin/course/resources/UserResource.java
@@ -1,22 +1,35 @@
 package com.felipezambrin.course.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.felipezambrin.course.entities.User;
+import com.felipezambrin.course.services.UserService;
 
 @RestController // vai ser um controller
 @RequestMapping(value="/users") // nome do recurso
 public class UserResource {
 	
-	@GetMapping //responde à requisição GET do HTTP
-	public ResponseEntity<User> findAll() {
-		User u = new User(1L, "Maria", "maria@gmail.com", "99999999", "12345");
-		return ResponseEntity.ok().body(u); // ok() retorna um BodyBuilder e .body() seta o corpo desse BodyBuilder, retornando-o
+	@Autowired
+	private UserService service;
+	
+	
+	@GetMapping //endpoint que responde à requisição GET do HTTP
+	public ResponseEntity<List<User>> findAll() {
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list); // ok() retorna um BodyBuilder e .body() seta o corpo desse BodyBuilder, retornando-o
 	}
 	
 	
-	
+	@GetMapping(value = "/{id}") // isso indica que a requisição vai aceitar um id na url
+	public ResponseEntity<User> findById(@PathVariable Long id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
 }
