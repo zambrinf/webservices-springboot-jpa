abae3b52ca852de911bf57c7d7116f9335f864a3
Felipe Zambrin <zambrinf@gmail.com>
2020-10-12 18:26:58 -0300

    OrderStatus enum

--- src/main/java/com/felipezambrin/course/entities/Order.java
+++ src/main/java/com/felipezambrin/course/entities/Order.java
@@ -12,6 +12,7 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.felipezambrin.course.entities.enums.OrderStatus;
 
 @Entity
 @Table(name = "tb_order")
@@ -26,15 +27,18 @@ public class Order implements Serializable {
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant moment;
 	
+	private Integer orderStatus; //colocar Integer apenas para tratamento interno
+	
 	@ManyToOne //muitos Order para um User
 	@JoinColumn(name = "client_id") //nome da chave entrangeira
 	private User client;
 	
 	public Order() {}
 
-	public Order(Long id, Instant moment, User client) {
+	public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
 		this.id = id;
 		this.moment = moment;
+		setOrderStatus(orderStatus);
 		this.client = client;
 	}
 	
@@ -52,6 +56,16 @@ public class Order implements Serializable {
 
 	public void setMoment(Instant moment) {
 		this.moment = moment;
+	}
+
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus);
+	}
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		if (orderStatus != null ) {
+			this.orderStatus = orderStatus.getCode();
+		}
 	}
 
 	public User getClient() {
