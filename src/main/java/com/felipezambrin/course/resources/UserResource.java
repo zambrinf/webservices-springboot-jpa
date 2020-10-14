73933a1041a1a7aeee54f2263196105fd9d7539e
Felipe Zambrin <zambrinf@gmail.com>
2020-10-13 21:17:37 -0300

    User insert

--- src/main/java/com/felipezambrin/course/resources/UserResource.java
+++ src/main/java/com/felipezambrin/course/resources/UserResource.java
@@ -1,18 +1,22 @@
 package com.felipezambrin.course.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.felipezambrin.course.entities.User;
 import com.felipezambrin.course.services.UserService;
 
-@RestController // vai ser um controller
+@RestController // vai ser um rest controller 
 @RequestMapping(value="/users") // nome do recurso
 public class UserResource {
 	
@@ -31,5 +35,14 @@ public class UserResource {
 	public ResponseEntity<User> findById(@PathVariable Long id) {
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User obj) { //@RequestBody para pegar o corpo da requisição com um Json
+		obj = service.insert(obj);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri(); // para fazer o código 201 e tambem retornar o endereço do objeto
+		
+		return ResponseEntity.created(uri).body(obj);
 	}
 }
--- src/main/java/com/felipezambrin/course/services/UserService.java
+++ src/main/java/com/felipezambrin/course/services/UserService.java
@@ -23,4 +23,8 @@ public class UserService {
 		Optional<User> obj = repository.findById(id); //findById do JpaRepository retorna um Optional
 		return obj.get();
 	}
+	
+	public User insert(User obj) {
+		return repository.save(obj);
+	}
 }
