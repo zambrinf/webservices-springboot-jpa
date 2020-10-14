b69d5ec5d8eb9cd3b1a2508e6f9aa9a814507558
Felipe Zambrin <zambrinf@gmail.com>
2020-10-13 22:02:44 -0300

    Exception handling - delete

--- src/main/java/com/felipezambrin/course/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/felipezambrin/course/resources/exceptions/ResourceExceptionHandler.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.felipezambrin.course.services.exceptions.DatabaseException;
 import com.felipezambrin.course.services.exceptions.ResourceNotFoundException;
 
 @ControllerAdvice //vai interceptar as exceções que acontecerem para executar o tratamento
@@ -18,6 +19,14 @@ public class ResourceExceptionHandler {
 	public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
 		String error = "Resource not found";
 		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError er = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(er);
+	}
+	
+	@ExceptionHandler(DatabaseException.class) //esse método intercepta qualquer exceção desse tipo
+	public ResponseEntity<StandardError> databaseNotFound(DatabaseException e, HttpServletRequest request) {
+		String error = "Database error";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
 		StandardError er = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
 		return ResponseEntity.status(status).body(er);
 	}
--- src/main/java/com/felipezambrin/course/services/UserService.java
+++ src/main/java/com/felipezambrin/course/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.felipezambrin.course.entities.User;
 import com.felipezambrin.course.repositories.UserRepository;
+import com.felipezambrin.course.services.exceptions.DatabaseException;
 import com.felipezambrin.course.services.exceptions.ResourceNotFoundException;
 
 @Service //registra a classe como um componente de serviço e pode ser injetado automaticamente, pode usar o @Component, @Repository, etc
@@ -23,7 +26,6 @@ public class UserService {
 	public User findById(Long id) {
 		
 		Optional<User> obj = repository.findById(id); //findById do JpaRepository retorna um Optional
-		
 		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
@@ -32,7 +34,13 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	
 	public User update(Long id, User obj) {
