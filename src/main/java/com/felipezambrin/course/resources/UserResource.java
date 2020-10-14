742eed12867ed006ef904f0208f8517b9254a8f8
Felipe Zambrin <zambrinf@gmail.com>
2020-10-13 21:24:22 -0300

    User delete

--- src/main/java/com/felipezambrin/course/resources/UserResource.java
+++ src/main/java/com/felipezambrin/course/resources/UserResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -32,7 +33,7 @@ public class UserResource {
 	
 	
 	@GetMapping(value = "/{id}") // isso indica que a requisição vai aceitar um id na url
-	public ResponseEntity<User> findById(@PathVariable Long id) {
+	public ResponseEntity<User> findById(@PathVariable Long id) { // @PathVariable para ele pegar o id do caminho da requisição
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
 	}
@@ -44,5 +45,11 @@ public class UserResource {
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri(); // para fazer o código 201 e tambem retornar o endereço do objeto
 		
 		return ResponseEntity.created(uri).body(obj);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build(); // o noContent retorna o código 204
 	}
 }
